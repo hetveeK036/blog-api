be079065404af80dc77ad0e92d240c814cc76b3c
ayushk048 <ayushkholiya@gmail.com>
2024-04-17 23:45:02 +0530

    added seeder

--- seeder/startup.sql
+++ seeder/startup.sql
@@ -1,55 +1,58 @@
--- -- Create the database if it doesn't exist
--- CREATE DATABASE IF NOT EXISTS StoryForge;
+-- Database: StoryForge
 
--- -- Use the database
--- USE StoryForge;
+-- DROP DATABASE IF EXISTS "StoryForge";
 
--- -- Create the stories table
--- CREATE TABLE IF NOT EXISTS Users (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     username VARCHAR(255) NOT NULL,
---     email VARCHAR(255) NOT NULL,
---     password VARCHAR(255) NOT NULL,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
-
--- Create the Posts table
--- CREATE TABLE IF NOT EXISTS Posts (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     username VARCHAR(255) NOT NULL,
---     title VARCHAR(255) NOT NULL,
---     description VARCHAR(255) NOT NULL,
---     img VARCHAR(255) NOT NULL,
---     cat VARCHAR(255) NOT NULL,
---     cat VARCHAR(255) NOT NULL,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
-
--- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS StoryForge;
-
--- Use the database
-USE StoryForge;
-
--- Create the Users table
-CREATE TABLE IF NOT EXISTS Users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Create the Posts table
-CREATE TABLE IF NOT EXISTS Posts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    img VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL, -- Changed to "category" instead of "cat"
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE DATABASE "StoryForge"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_India.1252'
+    LC_CTYPE = 'English_India.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
 
 
+-- Table: public.users
 
+-- DROP TABLE IF EXISTS public.users;
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id integer NOT NULL,
+    username character varying COLLATE pg_catalog."default" NOT NULL,
+    email character varying COLLATE pg_catalog."default" NOT NULL,
+    password character varying COLLATE pg_catalog."default" NOT NULL,
+    image character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.users
+    OWNER to postgres;
+
+
+-- Table: public.posts
+
+-- DROP TABLE IF EXISTS public.posts;
+
+CREATE TABLE IF NOT EXISTS public.posts
+(
+    id integer NOT NULL,
+    title character varying COLLATE pg_catalog."default" NOT NULL,
+    description character varying COLLATE pg_catalog."default" NOT NULL,
+    image character varying COLLATE pg_catalog."default" NOT NULL,
+    category character varying COLLATE pg_catalog."default" NOT NULL,
+    user_id integer NOT NULL,
+    CONSTRAINT posts_pkey PRIMARY KEY (id),
+    CONSTRAINT user_id FOREIGN KEY (user_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.posts
+    OWNER to postgres;
